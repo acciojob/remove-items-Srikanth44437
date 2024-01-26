90a5165f24e70a8ac60f2c82a77cb9362d590f13
Srikanth44437 <113106500+Srikanth44437@users.noreply.github.com>
2024-01-26 13:08:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+//your JS code here. If requi
+document.addEventListener("DOMContentLoaded", function(){
+	var removeButton = document.getElementById("removeButton");
+
+	removeButton.addEventListener("click", function(){
+		var colorselect = document.getElementById("colorSelect");
+
+		colorselect.remove(colorselect.startedIndex);
+	})
+})
